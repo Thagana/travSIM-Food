74746403cbc4541eab3a2950745885f8ea927166
Thagana <samuelmthwa79@gmail.com>
2020-05-02 01:56:07 +0200

    :tada: Root Component

--- App.js
+++ App.js
@@ -16,6 +16,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { useStoreState, createStore, StoreProvider } from "easy-peasy";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 import STORE from "./src/Store/model";
 const store = createStore(STORE);
 
@@ -25,13 +27,72 @@ import LoginScreen from "./src/screens/LoginScreen";
 import SignUp from "./src/screens/Register";
 import FeedScreen from "./src/screens/FeedStack";
 import ProfileScreen from "./src/screens/ProfileStack";
-import DashboardScreen from "./src/screens/DashboardScreen";
+import SearchScreen from "./src/screens/Search";
+import Cartigory from "./src/screens/Cartigory";
+import MoreInfo from "./src/screens/MoreInfo";
 
+const HomeStack = createStackNavigator();
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Explore" component={FeedScreen} />
+      <HomeStack.Screen name="More Info" component={MoreInfo} />
+    </HomeStack.Navigator>
+  );
+};
+
+const AppTabs = createBottomTabNavigator();
+const AppTabsScreeen = () => {
+  return (
+    <AppTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "App") {
+            iconName = "explore";
+          } else if (route.name === "Search") {
+            iconName = "search";
+          } else if (route.name === "Profile") {
+            iconName = "account-circle";
+          } else if (route.name === "Cartigory") {
+            iconName = "list";
+          }
+
+          // You can return any component that you like here!
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <AppTabs.Screen name="App" component={HomeStackScreen} />
+      <AppTabs.Screen name="Cartigory" component={Cartigory} />
+      <AppTabs.Screen name="Search" component={SearchScreen} />
+      <AppTabs.Screen name="Profile" component={ProfileScreen} />
+    </AppTabs.Navigator>
+  );
+};
+
+const FeedStack = createStackNavigator();
+const FeedStackScreen = () => {
+  return (
+    <FeedStack.Navigator
+      screenOptions={{
+        headerTitleAlign: "center",
+      }}
+      headerMode="none"
+    >
+      <FeedStack.Screen name="Explore" component={AppTabsScreeen} />
+    </FeedStack.Navigator>
+  );
+};
 const Drawer = createDrawerNavigator();
 const DrawerScreen = () => {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Feed" component={FeedScreen} />
+      <Drawer.Screen name="Menu" component={FeedStackScreen} />
     </Drawer.Navigator>
   );
 };
@@ -39,7 +100,13 @@ const DrawerScreen = () => {
 const AuthStack = createStackNavigator();
 const AuthStackScreen = (props) => {
   return (
-    <AuthStack.Navigator headerMode="none">
+    <AuthStack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        title: "",
+        headerTitleAlign: "center",
+      }}
+    >
       <AuthStack.Screen name="Welcome" component={WelcomeScreen} />
       <AuthStack.Screen name="Login" component={LoginScreen} />
       <AuthStack.Screen name="SignUp" component={SignUp} />
